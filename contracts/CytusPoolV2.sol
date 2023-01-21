7d38d4534e3d60f22bb26c4db3bdd3d97e581d5d
0xtbill <dgy@tprotocol.io>
2023-01-22 00:35:12 +0900

    chore: remove kyc

--- contracts/CytusPoolV2.sol
+++ contracts/CytusPoolV2.sol
@@ -6,42 +6,13 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-import "./tools/ERC1820Client.sol";
 import "./tools/DomainAware.sol";
-import "./interface/ERC1820Implementer.sol";
 import "./roles/MinterRole.sol";
-import "./extensions/tokenExtensions/IERC1400TokensValidator.sol";
 
 import "hardhat/console.sol";
 
-interface IExtensionTypes {
-    enum CertificateValidation {
-        None,
-        NonceBased,
-        SaltBased
-    }
-}
-
-abstract contract Extension is IExtensionTypes {
-    function registerTokenSetup(
-        address token,
-        CertificateValidation certificateActivated,
-        bool allowlistActivated,
-        bool blocklistActivated,
-        bool granularityByPartitionActivated,
-        bool holdsActivated,
-        address[] calldata operators
-    ) external virtual;
-
-    function addCertificateSigner(address token, address account)
-        external
-        virtual;
-}
 
 contract CytusPoolV2 is
-    ERC1820Client,
-    ERC1820Implementer,
-    IExtensionTypes,
     DomainAware,
     AccessControlUpgradeable,
     ERC20Upgradeable
@@ -49,24 +20,11 @@ contract CytusPoolV2 is
     using SafeMathUpgradeable for uint256;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    string internal constant ERC1400_TOKENS_VALIDATOR =
-        "ERC1400TokensValidator";
-    string internal constant ERC1400_INTERFACE_NAME = "ERC1400Token";
-    string internal constant ERC20_INTERFACE_NAME = "ERC20Token";
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant KYC_MANAGER_ROLE = keccak256("KYC_MANAGER_ROLE");
     bytes32 public constant POOL_MANAGER_ROLE = keccak256("POOL_MANAGER_ROLE");
 
     // This token's decimals is 18, USDC's decimals is 6, so the INITIAL_CTOKEN_TO_UNDERLYING is 10**(18-6)
     uint256 public constant INITIAL_CTOKEN_TO_UNDERLYING = 10**12;
-
-    // verificationMode: 0 => no verification, 1 => allowlist, 2 => allow all
-    enum VerificationMode {
-        CERTIFICATE,
-        ALLOW_LIST,
-        ALLOW_ALL,
-        DISABLED
-    }
 
     mapping(address => uint256) public cTokenBalances;
 
@@ -79,10 +37,6 @@ contract CytusPoolV2 is
     address public vault;
     // Treasury, used to receive USDC from user when buy cToken.
     address public treasury;
-
-    VerificationMode public verificationMode;
-
-    mapping(address => bool) public allowList;
 
     // withdrawFeeRate: 0.1% => 100000 (10 ** 5)
     // withdrawFeeRate: 10% => 10000000 (10 ** 7)
@@ -117,66 +71,22 @@ contract CytusPoolV2 is
     function initialize(
         string memory name,
         string memory symbol,
-        address extension,
         address admin,
-        address certificateSigner,
-        address[] memory controllers,
         IERC20Upgradeable _underlyingToken,
         uint256 _capitalLowerBound,
         address _treasury,
         address _vault
     ) public initializer {
-        require(
-            extension != address(0),
-            "CytusPoolV2: extension is zero address"
-        );
 
         AccessControlUpgradeable.__AccessControl_init();
         ERC20Upgradeable.__ERC20_init(name, symbol);
         DomainAware.__DomainAware_init();
 
-        Extension(extension).registerTokenSetup(
-            address(this), // token
-            CertificateValidation.NonceBased, // certificateActivated
-            true, // allowlistActivated
-            true, // blocklistActivated
-            true, // granularityByPartitionActivated
-            true, // holdsActivated
-            controllers // token controllers
-        );
-
-        if (certificateSigner != address(0)) {
-            Extension(extension).addCertificateSigner(
-                address(this),
-                certificateSigner
-            );
-        }
-
-        // Register contract in ERC1820 registry
-        ERC1820Client.setInterfaceImplementation(
-            ERC1400_INTERFACE_NAME,
-            address(this)
-        );
-        ERC1820Client.setInterfaceImplementation(
-            ERC20_INTERFACE_NAME,
-            address(this)
-        );
-        ERC1820Client.setInterfaceImplementation(
-            ERC1400_TOKENS_VALIDATOR,
-            extension
-        );
-
-        // Indicate token verifies ERC1400 and ERC20 interfaces
-        ERC1820Implementer._setInterface(ERC1400_INTERFACE_NAME); // For migration
-        ERC1820Implementer._setInterface(ERC20_INTERFACE_NAME); // For migration
-
         // TODO: revisit.
-        _setRoleAdmin(KYC_MANAGER_ROLE, ADMIN_ROLE);
         _setRoleAdmin(POOL_MANAGER_ROLE, ADMIN_ROLE);
 
         require(admin != address(0), "103");
         _setupRole(ADMIN_ROLE, admin);
-        _setupRole(KYC_MANAGER_ROLE, admin);
         _setupRole(POOL_MANAGER_ROLE, admin);
 
         underlyingToken = _underlyingToken;
@@ -185,7 +95,6 @@ contract CytusPoolV2 is
         vault = _vault;
         treasury = _treasury;
 
-        verificationMode = VerificationMode.CERTIFICATE;
         minAPR = 0;
         maxAPR = 10**7;
     }
@@ -239,14 +148,6 @@ contract CytusPoolV2 is
     }
 
 
-    function setVerificationMode(VerificationMode _verificationMode)
-        external
-        onlyRole(POOL_MANAGER_ROLE)
-    {
-        verificationMode = _verificationMode;
-    }
-
-
     function setAprRange(uint256 _minAPR, uint256 _maxAPR)
         external
         onlyRole(POOL_MANAGER_ROLE)
@@ -259,20 +160,6 @@ contract CytusPoolV2 is
     /* -------------------------- End of Pool Settings -------------------------- */
 
 
-
-
-    /* -------------------------------------------------------------------------- */
-    /*                                KYC Settings                                */
-    /* -------------------------------------------------------------------------- */
-
-    function updateAllowList(address[] calldata _allowList, bool _allow)
-        external
-        onlyRole(KYC_MANAGER_ROLE)
-    {
-        for (uint256 i = 0; i < _allowList.length; i++) {
-            allowList[_allowList[i]] = _allow;
-        }
-    }
 
     /* --------------------------- End of KYC Settings -------------------------- */
 
@@ -325,45 +212,10 @@ contract CytusPoolV2 is
         _;
     }
 
-    function validateBuyCertificate(uint256 amount, bytes calldata data)
-        internal
-    {
-        address validatorImplementation = interfaceAddr(
-            address(this),
-            ERC1400_TOKENS_VALIDATOR
-        );
-
-        IERC1400TokensValidator(validatorImplementation).tokensToValidate(
-            msg.data,
-            "partition",
-            msg.sender,
-            address(0),
-            msg.sender,
-            amount,
-            data,
-            ""
-        );
-    }
-
-    modifier verifyAccess(uint256 amount, bytes calldata data) {
-        require(verificationMode != VerificationMode.DISABLED, "107");
-        if (
-            verificationMode == VerificationMode.ALLOW_ALL ||
-            (verificationMode == VerificationMode.ALLOW_LIST &&
-                allowList[msg.sender])
-        ) {
-            _;
-        } else {
-            validateBuyCertificate(amount, data);
-            _;
-        }
-    }
-
     // @param amount: the amount of underlying token, 1 USDC = 10**6
     // @param data: certificate data
-    function buy(uint256 amount, bytes calldata data)
+    function buy(uint256 amount)
         external
-        verifyAccess(amount, data)
         realizeReward
     {
         underlyingToken.safeTransferFrom(msg.sender, treasury, amount);
@@ -380,10 +232,8 @@ contract CytusPoolV2 is
     }
 
     // @param amount: the amount of cToken, 1 cToken = 10**18, which eaquals to 1 USDC (if not interest).
-    // @param data: certificate data
-    function sell(uint256 amount, bytes calldata data)
+    function sell(uint256 amount)
         external
-        verifyAccess(amount, data)
         realizeReward
     {
         require(amount <= cTokenBalances[msg.sender], "100");
@@ -409,9 +259,8 @@ contract CytusPoolV2 is
         totalPendingWithdrawals = totalPendingWithdrawals.add(underlyingAmount);
     }
 
-    function withdrawUnderlyingToken(uint256 amount, bytes calldata data)
+    function withdrawUnderlyingToken(uint256 amount)
         external
-        verifyAccess(amount, data)
     {
         require(pendingWithdrawals[msg.sender] >= amount, "105");
         require(underlyingToken.balanceOf(vault) >= amount, "106");
@@ -454,21 +303,6 @@ contract CytusPoolV2 is
         return cTokenBalances[_owner];
     }
 
-    /* ---------------- Below are only enabled in allowlist mode ---------------- */
-    modifier verifyERC20Access(address from, address to) {
-        require(verificationMode != VerificationMode.DISABLED, "108");
-        if (verificationMode == VerificationMode.ALLOW_ALL) {
-            _;
-        } else if (verificationMode == VerificationMode.ALLOW_LIST) {
-            require(
-                allowList[from] && allowList[to],
-                "sender or receiver not in allow list"
-            );
-            _;
-        } else {
-            revert("not allowed in this verification mode");
-        }
-    }
 
     function _transfer(
         address from,
@@ -496,7 +330,6 @@ contract CytusPoolV2 is
     function transfer(address to, uint256 amount)
         public
         override
-        verifyERC20Access(msg.sender, to)
         returns (bool)
     {
         address owner = _msgSender();
@@ -508,7 +341,6 @@ contract CytusPoolV2 is
         public
         view
         override
-        verifyERC20Access(owner, spender)
         returns (uint256)
     {
         return super.allowance(owner, spender);
@@ -517,7 +349,6 @@ contract CytusPoolV2 is
     function approve(address spender, uint256 amount)
         public
         override
-        verifyERC20Access(msg.sender, spender)
         returns (bool)
     {
         return super.approve(spender, amount);
@@ -527,7 +358,7 @@ contract CytusPoolV2 is
         address from,
         address to,
         uint256 amount
-    ) public override verifyERC20Access(from, to) returns (bool) {
+    ) public override returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
         _transfer(from, to, amount);
